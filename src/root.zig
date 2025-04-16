096a9159c0964aeaebef08b27f65565ac79a90d4
PierreLgol <plgol.perso@gmail.com>
2025-04-16 17:51:55 +0200

    starting to play with NFA

--- src/root.zig
+++ src/root.zig
@@ -2,6 +2,7 @@ const std = @import("std");
 pub const Token = @import("regex/Token.zig").Token;
 pub const Lexer = @import("regex/Lexer.zig").Lexer;
 pub const Parser = @import("regex/Parser.zig").Parser;
+pub const NFA = @import("regex/NFA.zig").Nfa;
 pub const Ast = @import("regex/Ast.zig").Ast;
 
 comptime {
@@ -9,4 +10,5 @@ comptime {
     std.testing.refAllDecls(Lexer);
     std.testing.refAllDecls(Parser);
     std.testing.refAllDecls(Ast);
+    std.testing.refAllDecls(NFA);
 }
